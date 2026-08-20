a2e2f6bb8b8baedc3673a05bddeadc7609d9f1cb
zhengzhuolun <alien@zhengzhuolundeMacBook-Air.local>
2026-08-20 22:28:09 +0800

    Add GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
+    base: process.env.GITHUB_PAGES === 'true' ? '/APP/' : '/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
